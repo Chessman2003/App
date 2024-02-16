0ebbbaa0015e98a569cc58eedf9efe34daa79483
Ivancov Alexander <ivancovae@kodeks.ru>
2024-02-16 18:32:18 +0300

    createBug

--- src/Components/App/App.tsx
+++ src/Components/App/App.tsx
@@ -12,13 +12,13 @@ export const App = () => {
             <Dropdown options={
                 [
                     { label: 'Опция 1', value: 'option1' },
-                    { label: 'Опция 2', value: 'option2', disabled: true },
+                    { label: 'Опция 5', value: 'option2', disabled: true },
                     { label: 'Опция 3 при наличии', value: 'option3' },
-                    { label: 'Опция 4', value: 'option4' },
+                    { label: 'Опция 2', value: 'option4' },
                     { label: 'Опция 5', value: 'option5' },
                     { label: 'Опция 6', value: 'option6' },
                     { label: 'Опция 7', value: 'option7', disabled: true },
-                    { label: 'Опция 8', value: 'option8' },
+                    { label: 'Опция 123', value: 'option8' },
                     { label: 'Опция 9', value: 'option9' },
                     { label: 'Опция 10 ', value: 'option10', disabled: true },
                     { label: 'Опция 11', value: 'option11' },
